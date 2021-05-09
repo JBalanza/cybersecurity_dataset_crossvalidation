dceb9b52ed83abd2ed3df827d9e3565fcf9705ce
Javier <lavier@localhost>
2021-05-09 20:14:48 +0200

    add status file to cicflowprocessor.sh

--- cicflowprocessor.sh
+++ cicflowprocessor.sh
@@ -1,12 +1,19 @@
-#/bin/bash
+#!/bin/bash
 
 root_dir="${1}"
 pcap_files=()
+log_file="./parsed.txt"
 readarray -t pcap_files < <(find "${root_dir}" -type f -name '*.pcap')
 
 for file in "${pcap_files[@]}"
 do
 	prev_dir="$(dirname ${file})"
-	random_name="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 5)"
-	cicflowmeter -f "${file}" -c "${prev_dir}/${random_name}.csv"
+	random_name="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 7)"
+	if grep -xq "${file}" "${log_file}"
+	then
+	  echo "Already parsed this file ${file}"
+	else
+	  cicflowmeter -f "${file}" -c "${prev_dir}/${random_name}.csv"
+	  echo "${file}" >> "${log_file}"
+	fi
 done
